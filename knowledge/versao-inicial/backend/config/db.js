715c21a486606b54f2c9e52f3bb2a4af2db716fb
edinaldopsj <edinaldopsj92@gmail.com>
2019-11-24 17:37:48 -0300

    Removed knowledge to due folder transfered into a whole project

--- knowledge/versao-inicial/backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const config = require('../knexfile.js')
-const knex = require('knex')(config)
-
-//Executa a migração no momento que rodar o backend
-knex.migrate.latest([config])
-
-module.exports = knex
